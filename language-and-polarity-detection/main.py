c9fef3ee8ea6359776ec2f3c4e75d9cfeff2d95e
Bojan Božić <boj.bozic@gmail.com>
2017-04-28 19:22:25 +0100

    initial commit

---
+++ language-and-polarity-detection/main.py
@@ -0,0 +1,21 @@
+import language_train_model as language
+import sentiment
+import ISO639_2 as codes
+import sys
+import argparse
+
+__LANG_PATH__ = 'data/languages/paragraphs'
+__SENT_PATH__ = 'data/movie_reviews/txt_sentoken'
+
+if __name__ == "__main__":
+   parser = argparse.ArgumentParser()
+   parser.add_argument('--ldata', help='Language data', default=__LANG_PATH__)
+   parser.add_argument('--sdata', help='Sentiment data', default=__SENT_PATH__)
+   args = vars(parser.parse_args())
+
+   sentence = input("Sentence: ")
+   language_results = language.get_language([sentence], args['ldata'])
+   for result in language_results:
+      print('The language of the sentence is '+codes.ISO639_2[result]+'.')
+      if(result == 'en'):
+         sentiment.sentiment(result, args['sdata'])
